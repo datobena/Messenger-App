9ba9876b7fb334eb216139c850c91f4d2599b843
bena <dbena20@freeuni.edu.ge>
2023-07-15 14:30:19 +0400

    Fixed avatar loading problem after sign out.

--- app/src/main/java/ge/dbenadshis/messengerapp/MainActivity.kt
+++ app/src/main/java/ge/dbenadshis/messengerapp/MainActivity.kt
@@ -386,9 +386,15 @@ fun LogInPage(){
     }
 }
 
-private fun saveUserAndNavigate(navController: NavHostController, nickname: String, pass: String, work: String){
+private fun saveUserAndNavigate(
+    navController: NavHostController,
+    nickname: String,
+    pass: String,
+    work: String,
+    avatarURL: String = ""
+){
     sharedPreferences!!.edit().putString("nickname", nickname).putString("pass", pass).apply()
-    userViewModel.curUser = User(nickname, pass, work)
+    userViewModel.curUser = User(nickname, pass, work, avatarURL)
     navController.navigate(Screen.Home.route)
 }
 
@@ -396,7 +402,7 @@ fun signInAccount(navController: NavHostController, nickname: String, pass: Stri
     CoroutineScope(Dispatchers.Default).launch {
         userViewModel.checkUser(nickname, pass, object : UserRepositoryImpl.UserExistenceCallback {
             override fun onUserExists(user: User) {
-                saveUserAndNavigate(navController, nickname, pass, user.work)
+                saveUserAndNavigate(navController, nickname, pass, user.work, user.avatarURL)
             }
 
             override fun onUserDoesNotExist() {
